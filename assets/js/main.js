919f3ad8cacfa10abbcc00a09001b67b164474c5
MarcoGotti <mrc.gotti@gmail.com>
2023-11-27 20:03:10 +0000

    head commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,6 +12,10 @@ Iniziamo implementando il programma senza alcuna estetica: usando esclusivamente
 */
 
 
+//const utility
+const ticketElement = document.querySelector('.your-ticket');
+const priceElement = document.getElementById('total-price');
+
 //click to generate
 const buttonGenerate = document.getElementById('generate-btn');
 buttonGenerate.addEventListener('click', function(){
@@ -20,9 +24,7 @@ buttonGenerate.addEventListener('click', function(){
     const name = document.getElementById('Name').value;
     const surname = document.getElementById('Surname').value;
     const fullname = (name + ' ' + surname);
-
     const km = document.getElementById('wished-km').value;
-
     const age = document.getElementById('user-age').value;
 
     //calcola prezzo full
@@ -31,25 +33,22 @@ buttonGenerate.addEventListener('click', function(){
 
 
     //calcola prezzo secondo età
- if ((name.length < 1) || (surname.length < 1) || (km.length < 1) || (age.length < 1)/* || (surname = '') || (km = '') || (age = '') */) {
+ if ((name.length < 1) || (surname.length < 1) || (km.length < 1)) {
         //alert
-        const ticketElement = document.querySelector('.your-ticket');
         ticketElement.classList.add('d-none')
         alert('You must fill every space');
         
-    }else if (age < 18) {
+    }else if (age == 1) {
         //calcola prezzo e stampa ticket-type
         price = (price_full * 0.8);
         document.getElementById('priceType').innerText = ('Discount Under-18');
         //stampa biglietto con i dati
-        const ticketElement = document.querySelector('.your-ticket');
         console.log(ticketElement);
         ticketElement.classList.remove('d-none')
         console.log(fullname);
         document.getElementById('passenger').innerText = (fullname);
     
         console.log(price);
-        const priceElement = document.getElementById('total-price');
         priceElement.innerHTML = (`${price.toFixed(2)} €`);
 
         document.getElementById('wagon').innerText = (Math.floor(Math.random() * 10) +1);
@@ -59,19 +58,17 @@ buttonGenerate.addEventListener('click', function(){
         return Math.floor(Math.random() * (max - min) ) + min;
         }
 
-    } else if (age >= 65) {
+    } else if (age == 3) {
         //calcola prezzo e stampa ticket-type
         price = (price_full * 0.6)
         document.getElementById('priceType').innerText = ('Reduced over-65');
         //stampa biglietto
-        const ticketElement = document.querySelector('.your-ticket');
         console.log(ticketElement);
         ticketElement.classList.remove('d-none')
         console.log(fullname);
         document.getElementById('passenger').innerText = (fullname);
     
         console.log(price);
-        const priceElement = document.getElementById('total-price');
         priceElement.innerHTML = (`${price.toFixed(2)} €`);
 
         document.getElementById('wagon').innerText = (Math.floor(Math.random() * 10) +1);
@@ -86,14 +83,12 @@ buttonGenerate.addEventListener('click', function(){
         price = (price_full)
         document.getElementById('priceType').innerText = ('Standard');
         //stampa biglietto
-        const ticketElement = document.querySelector('.your-ticket');
         console.log(ticketElement);
         ticketElement.classList.remove('d-none')
         console.log(fullname);
         document.getElementById('passenger').innerText = (fullname);
     
         console.log(price);
-        const priceElement = document.getElementById('total-price');
         priceElement.innerHTML = (`${price.toFixed(2)} €`);
 
         document.getElementById('wagon').innerText = (Math.floor(Math.random() * 10) +1);
@@ -108,28 +103,12 @@ buttonGenerate.addEventListener('click', function(){
 //click to cancel
 document.getElementById('cancel-btn').addEventListener('click', 
 function () {
+    ticketElement.classList.add('d-none')
     document.getElementById('Name').value = '';
     document.getElementById('Surname').value = '';
-    document.getElementById('wished-km').value = '';   //??input type:"number"??
+    document.getElementById('wished-km').value = '';  
     document.getElementById('user-age').value = '';
     
 });
 
 
- /*  //stampa nome e cognome
-    console.log(fullname);
-    document.getElementById('passenger').innerText = (fullname);
-    
-    //stampa prezzo totale
-    console.log(price);
-    const priceElement = document.getElementById('total-price');
-    priceElement.innerHTML = (`${price.toFixed(2)} €`);
-
-    //stampa random carrozza
-    document.getElementById('wagon').innerText = (Math.floor(Math.random() * 10) +1);
-
-    //stampa random cp-code
-    document.getElementById('cp-code').innerHTML = getRndInteger(11111,98766)
-    function getRndInteger(min, max) {
-        return Math.floor(Math.random() * (max - min) ) + min;
-      } */
